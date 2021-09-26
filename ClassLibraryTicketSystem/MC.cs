f35f755798d6f1524c42af91d2742645643b9b2b
Paula Catalina Curca <paul6465@edu.easj.dk>
2021-09-26 13:10:47 +0200

    Task 3

---
+++ ClassLibraryTicketSystem/MC.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTicketSystem
+{
+    public class MC
+    {
+        public string Licenseplate { get; set; }
+       
+        public DateTime Date { get; set; }
+
+        public double Price()
+        {
+            return 125;
+            //The price is fixed at 125kr which must be returned
+        }
+
+        public string Vehicle()
+        {
+            return "MC";
+            //which returns the string "MC"
+        }
+    }
+}
---
+++ ClassLibraryTicketSystemTests/MCTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibraryTicketSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTicketSystem.Tests
+{
+    [TestClass()]
+    public class MCTests
+    {
+        [TestMethod()]
+        public void PriceTest()
+        {
+            MC c = new MC();
+            double expected = 125;
+
+            Assert.AreEqual(expected, c.Price());
+        }
+
+        [TestMethod()]
+        public void VehicleTest()
+        {
+            MC c = new MC();
+            string expected = "MC";
+            Assert.AreEqual(expected, c.Vehicle());
+        }
+    }
+}
