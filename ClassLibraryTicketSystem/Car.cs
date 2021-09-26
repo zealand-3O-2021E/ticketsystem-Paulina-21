b6fc9360641359e58c3337bfe8a74f7e7b3ed619
Paula Catalina Curca <paul6465@edu.easj.dk>
2021-09-26 12:56:40 +0200

    Task 2

---
+++ ClassLibraryTicketSystem/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTicketSystem
+{
+    /// <summary>
+    /// Car Class Object with Licenseplate & Date Properties and Price and VehicleType methods
+    /// </summary>
+    public class Car
+    {
+        /// <summary>
+        /// Property which stores the License plate number, string
+        /// </summary>
+        public string Licenseplate { get; set; }
+        /// <summary>
+        /// Property which stores a Date, type DateTime
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Method returning the price (double) of the Car, default 240
+        /// </summary>
+        /// <returns></returns>
+        public double Price()
+        {
+            return 240;
+            //The price is fixed at 240 which must be returned
+        }
+
+        /// <summary>
+        /// Method returning the Vehicle Type (string), default "Car"
+        /// </summary>
+        /// <returns></returns>
+        public string VehicleType()
+        {
+            return "Car";
+            //which returns the string "Car"
+        }
+    }
+}
---
+++ ClassLibraryTicketSystemTests/CarTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibraryTicketSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTicketSystem.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        [TestMethod()]
+        public void PriceTest()
+        {
+            Car c=new Car();
+            double expected = 240;
+
+            Assert.AreEqual(expected,c.Price());
+        }
+
+        [TestMethod()]
+        public void VehicleTypeTest()
+        {
+            Car c = new Car();
+            string expected = "Car";
+            Assert.AreEqual(expected,c.VehicleType());
+        }
+    }
+}
